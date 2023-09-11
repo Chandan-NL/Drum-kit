1ffb540bff9789e4234bb547a1469d97811d0b04
Chandan NL <cnl3475@gmail.com>
2023-09-11 20:08:27 +0530

    first commit in this project

---
+++ script.js
@@ -0,0 +1,56 @@
+
+// var Num = document.querySelectorAll(".drum").length;
+// for (var i = 0; i < Num; i++) {
+
+//     document.querySelectorAll(".drum")[i].addEventListener("click", ss);
+//     function ss() {
+
+//         var innerhtml = this.innerhtml;
+
+//         switch (innerhtml) {
+//             case "w":
+//                 var tom1 = new Audio("sounds/tom-1.mp3");
+//                 tom1.play();
+//                 break;
+//             case "a":
+//                 var tom2 = new Audio("sounds/tom-2.mp3");
+//                 tom2.play();
+//                 break;
+//             case "s":
+//                 var tom3 = new Audio("sounds/tom-3.mp3");
+//                 tom3.play();
+//                 break;
+
+//             case "d":
+//                 var tom1 = new Audio("sounds/tom-4.mp3");
+//                 tom4.play();
+//                 break;
+
+//             case "w":
+//                 var tom1 = new Audio("sounds/tom-1.mp3");
+//                 tom1.play();
+//                 break;
+
+//             case "j":
+//                 var tomj = new Audio("sounds/crash.mp3");
+//                 tomj.play();
+//                 break;
+
+//             case "k":
+//                 var tomk = new Audio("sounds/kick-bass.mp3");
+//                 tomk.play();
+//                 break;
+//             case "l":
+//                 var toml = new Audio("sounds/snare.mp3");
+//                 toml.play();
+//                 break;
+
+//             default: console.log(innerhtml);
+
+//         }
+//     }
+
+// }
+
+
+
